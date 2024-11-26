0117cb0ff8c3b68b51f4a157a79b2b028f56a145
sriyogitha2 <sriyogithaselvaraj2001@gmail.com>
2024-11-26 12:36:12 +0530

    Delete task_11 directory

--- task_11/TestData/data.py
+++
@@ -1,7 +0,0 @@
-class HRMData:
-    url="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    dashboard_url="https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
-    excel_file="C:\\Users\\ritha\\Desktop\\automation\\task_11\\TestData\\test_data.xlsx"
-    sheet_number="Sheet1"
-
-
--- task_11/TestLocators/locators.py
+++
@@ -1,4 +0,0 @@
-class HRMLocators:
-    username_locator='username'
-    password_locator='password'
-    loginbutton_locator='orangehrm-login-button'
